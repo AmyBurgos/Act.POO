9a19d2c990d3c065a57f02570a663ddf542d6d4f
AmyBurgos <amygallebur7@gmail.com>
2025-01-27 12:54:03 -0700

    actividad2

---
+++ Actividad1/src/main/java/com/mycompany/actividad1/Actividad1.java
@@ -0,0 +1,15 @@
+package com.mycompany.actividad1;
+public class Actividad1 {
+
+    public static void main(String[] args) {
+        int numero = 5;
+        double numero2 = 1.5;
+        boolean dato = false;
+        string texto = "";
+        int arreglo[] = {5, 1, 8, 3, 20, 15};
+        int arreglo2 [] = new int[7];
+        for (int i = 0; i < 10; i++) {
+            
+        
+    }
+}
